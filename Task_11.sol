b3151c5211e05a14f5c948d39553d63d48063c98
Yaroslav Chetskiy <chetskiy.yaroslav@yandex.ru>
2025-12-24 18:44:09 +0300

    task11

---
+++ Task_11.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Task_11 {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the contract owner");
+        _;
+    }
+
+    function transferOwnership(address newOwner) external onlyOwner {
+        require(newOwner != address(0), "New owner is the zero address");
+        owner = newOwner;
+    }
+}
