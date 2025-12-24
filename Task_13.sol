6a6370f98bb10492c141ed1618f571d797345a1e
Yaroslav Chetskiy <chetskiy.yaroslav@yandex.ru>
2025-12-24 19:00:17 +0300

    task13

--- Task_13.sol
+++ Task_13.sol
@@ -1,40 +1,45 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+
 contract Task_13 {
-    function assertExample(uint256 _value) public pure {
-        assert(_value != 0);
-    }
+   function assertExample(uint256 _value) public pure {
+       assert(_value != 0);
+   }
 
-    function revertExample(bool _condition) public pure {
-        if (!_condition) {
-            revert("Invalid condition");
-        }
-    }
 
-    function ifElseExample(uint256 _value) public pure returns (string memory) {
-        if (_value > 10) {
-            return "Value is greater than 10";
-        } else {
-            return "Value is 10 or less";
-        }
-    }
+   function revertExample(bool _condition) public pure {
+       if (!_condition) {
+           revert("Invalid condition");
+       }
+   }
 
-    function elseIfExample(uint256 _value) public pure returns (string memory) {
-        if (_value > 10) {
-            return "Value is greater than 10";
-        } else if (_value == 10) {
-            return "Value is exactly 10";
-        } else {
-            return "Value is less than 10";
-        }
-    }
 
-    function ternaryExample(uint256 _value) public pure returns (string memory) {
-        if (_value > 10) {
-            return "Value is greater than 10";
-        } else {
-            return "Value is 10 or less";
-        }
-    }
+   function ifElseExample(uint256 _value) public pure returns (string memory) {
+       if (_value > 10) {
+           return "Value is greater than 10";
+       } else {
+           return "Value is 10 or less";
+       }
+   }
+
+
+   function elseIfExample(uint256 _value) public pure returns (string memory) {
+       if (_value > 10) {
+           return "Value is greater than 10";
+       } else if (_value == 10) {
+           return "Value is exactly 10";
+       } else {
+           return "Value is less than 10";
+       }
+   }
+
+
+   function ternaryExample(uint256 _value) public pure returns (string memory) {
+       if (_value > 10) {
+           return "Value is greater than 10";
+       } else {
+           return "Value is 10 or less";
+       }
+   }
 }
