72e12cf44073e4fa9ffbf90e721e14ccf6c21772
Yaroslav Chetskiy <chetskiy.yaroslav@yandex.ru>
2025-12-24 18:42:07 +0300

    task10

---
+++ Task_10.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Task_10 {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function isFutureTimestamp(uint256 _timestamp) public view returns (bool) {
+        return _timestamp > block.timestamp;
+    }
+
+    function isPastTimestamp(uint256 _timestamp) public view returns (bool) {
+        return _timestamp < block.timestamp;
+    }
+}
