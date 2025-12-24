19aa2622febc500f05881130c6cb12c68a3e1983
Yaroslav Chetskiy <chetskiy.yaroslav@yandex.ru>
2025-12-24 18:38:17 +0300

    task08

---
+++ Task_08.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Task_08 {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function getDynamicByteArrayLength(bytes memory _data) public pure returns (uint256) {
+        return _data.length;
+    }
+
+    function getDynamicByteArrayElement(bytes calldata _data, uint256 _index)
+        external
+        pure
+        returns (bytes1)
+    {
+        require(_index < _data.length, "Index out of bounds");
+        return _data[_index];
+    }
+}
