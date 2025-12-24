27ca6742e0b8dea02706628b3b0ee67541f85bd4
Yaroslav Chetskiy <chetskiy.yaroslav@yandex.ru>
2025-12-24 19:10:33 +0300

    lb6/task2

--- lb6/Task_03.sol
+++ lb6/Task_03.sol
@@ -3,13 +3,12 @@ pragma solidity ^0.8.0;
 
 contract Task_03 {
     function generateFactorial(uint256 n) external pure returns (uint256) {
-        require(n <= 57, "n is too large (uint256 overflow)");
-
         uint256 result = 1;
+        uint256 i = 1;
 
-        while (n > 1) {
-            result *= n;
-            n--;
+        while (i <= n) {
+            result *= i;
+            i++;
         }
 
         return result;
