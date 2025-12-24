c89df9a3c45c3ddc673e53dfec6fdcd34da3608f
Yaroslav Chetskiy <chetskiy.yaroslav@yandex.ru>
2025-12-24 19:20:34 +0300

    lb6/task6

--- lb6/Task_06.sol
+++ lb6/Task_06.sol
@@ -4,42 +4,58 @@ pragma solidity ^0.8.0;
 contract Task_06 {
     address public owner;
 
-    mapping(uint256 => string) private colors;
-
-    constructor() {
-        owner = msg.sender;
-    }
+    mapping(uint8 => string) private colors;
+    uint8 public constant COLORS_COUNT = 7;
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Not the contract owner");
         _;
     }
 
-    function addColor(uint256 index, string memory color) public {
-        require(index < 7, "Index must be 0..6");
+    constructor() {
+        owner = msg.sender;
+
+        string[] memory defaults = new string[](COLORS_COUNT);
+        defaults[0] = "Red";
+        defaults[1] = "Orange";
+        defaults[2] = "Yellow";
+        defaults[3] = "Green";
+        defaults[4] = "Blue";
+        defaults[5] = "Indigo";
+        defaults[6] = "Violet";
+
+        for (uint8 i = 0; i < COLORS_COUNT; i++) {
+            colors[i] = defaults[i];
+        }
+    }
+
+    function addColor(uint8 index, string memory color) public {
+        require(index < COLORS_COUNT, "Index out of range");
         colors[index] = color;
     }
 
-    function getColor(uint256 index) public view returns (string memory) {
-        require(index < 7, "Index must be 0..6");
+    function getColor(uint8 index) public view returns (string memory) {
+        require(index < COLORS_COUNT, "Index out of range");
         return colors[index];
     }
 
+    function colorExists(uint8 index) public view returns (bool) {
+        require(index < COLORS_COUNT, "Index out of range");
+        return bytes(colors[index]).length > 0;
+    }
+
+    function removeColor(uint8 index) public onlyOwner {
+        require(index < COLORS_COUNT, "Index out of range");
+        delete colors[index];
+    }
+
     function getAllColors() public view returns (string[] memory) {
-        string;
-        for (uint256 i = 0; i < 7; i++) {
+        string[] memory result = new string[](COLORS_COUNT);
+
+        for (uint8 i = 0; i < COLORS_COUNT; i++) {
             result[i] = colors[i];
         }
+
         return result;
-    }
-
-    function colorExists(uint256 index) public view returns (bool) {
-        require(index < 7, "Index must be 0..6");
-        return bytes(colors[index]).length != 0;
-    }
-
-    function deleteColor(uint256 index) public onlyOwner {
-        require(index < 7, "Index must be 0..6");
-        delete colors[index];
     }
 }
