fb0d9651cf27c9c44253536b6c652a756b45cd8c
Yaroslav Chetskiy <chetskiy.yaroslav@yandex.ru>
2025-12-25 15:19:55 +0300

    lb6/task07

--- lb6/Task_07.sol
+++ lb6/Task_07.sol
@@ -2,49 +2,72 @@
 pragma solidity ^0.8.0;
 
 contract Task_07 {
-    uint8 public constant COLORS_COUNT = 7;
-    mapping(uint8 => string) private colors;
-
-    function addColor(uint8 index, string calldata color) external {
-        require(index < COLORS_COUNT, "Index out of range");
-        require(bytes(colors[index]).length == 0, "Color already exists");
-        colors[index] = color;
+    struct Athlete {
+        string name;
+        uint256 result;
     }
 
-    function getColor(uint8 index) external view returns (string memory) {
-        require(index < COLORS_COUNT, "Index out of range");
-        return colors[index];
+    mapping(uint => Athlete) public athletes;
+    address public owner;
+    uint public constant ATHLETES_COUNT = 3;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
     }
 
-    function getAllColors() external view returns (string[] memory) {
-        string[] memory result = new string[](COLORS_COUNT);
-        for (uint8 i = 0; i < COLORS_COUNT; i++) {
-            result[i] = colors[i];
+    constructor() {
+        owner = msg.sender;
+        addAthlete(0, "John Doe", 0);
+        addAthlete(1, "Jane Smith", 0);
+        addAthlete(2, "Mike Johnson", 0);
+    }
+
+    function addAthlete(uint index, string memory athlete, uint256 result) public {
+        athletes[index] = Athlete(athlete, result);
+    }
+
+    function addAthleteExternal(uint index, string calldata athlete, uint256 result) external {
+        athletes[index] = Athlete(athlete, result);
+    }
+
+    function getAthlete(uint index) external view returns (string memory, uint256) {
+        Athlete storage a = athletes[index];
+        return (a.name, a.result);
+    }
+
+    function getAllAthletes() public view returns (string[] memory, uint256[] memory) {
+        string[] memory names = new string[](ATHLETES_COUNT);
+        uint256[] memory results = new uint256[](ATHLETES_COUNT);
+
+        for (uint i = 0; i < ATHLETES_COUNT; i++) {
+            names[i] = athletes[i].name;
+            results[i] = athletes[i].result;
         }
-        return result;
+
+        return (names, results);
     }
 
-    function updateColor(uint8 index, string calldata newColor) external {
-        require(index < COLORS_COUNT, "Index out of range");
-        require(bytes(colors[index]).length != 0, "Color does not exist");
-        colors[index] = newColor;
+    function updateAthlete(uint index, string calldata newAthlete, uint256 newResult) public {
+        Athlete storage a = athletes[index];
+        a.name = newAthlete;
+        a.result = newResult;
     }
 
-    function colorExists(uint8 index) external view returns (bool) {
-        require(index < COLORS_COUNT, "Index out of range");
-        return bytes(colors[index]).length != 0;
+    function athleteExists(uint index) public view returns (bool) {
+        return bytes(athletes[index].name).length > 0;
     }
 
-    function deleteColor(uint8 index) external {
-        require(index < COLORS_COUNT, "Index out of range");
-        require(bytes(colors[index]).length != 0, "Color does not exist");
-        delete colors[index];
+    function removeAthlete(uint index) public onlyOwner {
+        delete athletes[index];
     }
 
-    function replaceAllColors(string[] calldata newColors) external {
-        require(newColors.length == COLORS_COUNT, "Invalid colors length");
-        for (uint8 i = 0; i < COLORS_COUNT; i++) {
-            colors[i] = newColors[i];
+    function replaceAllAthletes(string[] calldata newAthletes, uint256[] calldata newResults) public onlyOwner {
+        require(newAthletes.length == ATHLETES_COUNT, "Must provide exactly 3 athletes");
+        require(newResults.length == ATHLETES_COUNT, "Must provide exactly 3 results");
+
+        for (uint i = 0; i < ATHLETES_COUNT; i++) {
+            athletes[i] = Athlete(newAthletes[i], newResults[i]);
         }
     }
 }
