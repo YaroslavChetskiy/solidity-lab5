2f2c1c6b1a27ede4fa1f432f25b027f2a0db4a50
Yaroslav Chetskiy <chetskiy.yaroslav@yandex.ru>
2025-12-25 03:33:55 +0300

    lb6/task09

--- lb6/Task_09.sol
+++ lb6/Task_09.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 
 contract Task_09 {
@@ -8,15 +8,15 @@ contract Task_09 {
     mapping(uint256 => string) public users;
     uint256 public userCount;
 
-    function addUser(string calldata name) external {
+    function addUser(string memory name) external {
         userCount++;
         users[userCount] = name;
-        emit UserAdded(userCount, "User added");
+        emit UserAdded(userCount, "User added successfully.");
     }
 
-    function removeUser(uint256 userId, string calldata message) external {
-        require(bytes(users[userId]).length != 0, "User does not exist");
+    function removeUser(uint256 userId) external {
+        require(bytes(users[userId]).length != 0, "User does not exist.");
         delete users[userId];
-        emit UserRemoved(userId, message);
+        emit UserRemoved(userId, "User removed successfully.");
     }
 }
